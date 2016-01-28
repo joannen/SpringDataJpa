e2897c187df05969e9d317fb31417ece628d26d0
Anna Andersson <anna@snigelfart.se>
2016-01-28 14:17:22 +0100

    equal and hashcode in workitem

--- src/main/java/se/threegirlsoneflop/springdatajpa/model/WorkItem.java
+++ src/main/java/se/threegirlsoneflop/springdatajpa/model/WorkItem.java
@@ -13,7 +13,7 @@ public class WorkItem extends AbstractEntity{
     @Column(nullable = false)
     private String description;
 
-    @Column
+    @Column(nullable = false)
     private String status;
 
     @OneToOne
@@ -58,24 +58,34 @@ public class WorkItem extends AbstractEntity{
         this.issue = issue;
     }
 
-
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
 
         WorkItem workItem = (WorkItem) o;
 
         if (description != null ? !description.equals(workItem.description) : workItem.description != null)
             return false;
-        return status != null ? status.equals(workItem.status) : workItem.status == null;
+        if (status != null ? !status.equals(workItem.status) : workItem.status != null) return false;
+        return issue != null ? issue.equals(workItem.issue) : workItem.issue == null;
 
     }
 
+    @Override
     public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (description != null ? description.hashCode() : 0);
+        int result = description != null ? description.hashCode() : 0;
         result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (issue != null ? issue.hashCode() : 0);
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return "WorkItem{" +
+                "description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                ", issue=" + issue +
+                '}';
     }
 }
