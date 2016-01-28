2fa2d97c7ab5d5b846ad1c7433ca98273a5369a6
Joanne Nori <joanne_nori@hotmail.com>
2016-01-28 11:08:13 +0100

    second first commit

---
+++ src/main/java/se/threegirlsoneflop/springdatajpa/config/AppConfig.java
@@ -0,0 +1,63 @@
+package se.threegirlsoneflop.springdatajpa.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+
+/**
+ * Created by joanne on 28/01/16.
+ */
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("se.threegirlsoneflop.repository")
+public class AppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("com.mysql.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mysql://localhost/springdatajpa");
+        config.setUsername("root");
+        config.setPassword("hannele1");
+
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public JpaTransactionManager jpaTransactionManager(EntityManagerFactory factory){
+
+        return new JpaTransactionManager(factory);
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setGenerateDdl(true);
+
+        return adapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(dataSource());
+        factory.setJpaVendorAdapter(jpaVendorAdapter());
+        factory.setPackagesToScan("se.threegirlsoneflop.springdatajpa.model");
+
+        return factory;
+    }
+}
