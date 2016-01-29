0afc0ae3fa01a314d42cba6949872ed9f3fff656
jl77 <jo.lantz@icloud.com>
2016-01-29 08:03:13 +0100

    Skapat UserRepository och lagt in metoder Skapaat Entity med relevanta medlemmar Implementerat Equals och Hashcode för User Har gjort kommentarer som jag har funderingar kring, se i User-klassen

--- src/main/java/se/threegirlsoneflop/springdatajpa/model/User.java
+++ src/main/java/se/threegirlsoneflop/springdatajpa/model/User.java
@@ -1,13 +1,113 @@
 package se.threegirlsoneflop.springdatajpa.model;
 
-import javax.persistence.Entity;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import se.threegirlsoneflop.springdatajpa.status.Status;
+
+import javax.annotation.Generated;
+import javax.persistence.*;
 
 /**
  * Created by joanne on 28/01/16.
  */
 @Entity
-public class User {
-    ffffff
+public class User extends AbstractEntity {
 
+    @Id
+    @GeneratedValue
+    private Long Id;
+    @Column
+    private String userNumber;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String status;
 
+    @ManyToOne
+    private Team team;
+
+    protected User() {}
+
+    /*Vad gäller team initialisering. Hur ska jag initialisera denna? För mig känns det konstigt att man
+    skulle behöva ange teamName när man skapar en User. En User kan väl ibland inte tillhöra ett team?
+    Jag vill i konstruktorn kunna skriva this.team = new Team() och sen ha en setTeam() som tar emot det team som addUser
+    i Team-klassen returnerar. Tycker ni att det skulle vara ok att införa ytterligare en konstruktor i Team? Se bort-
+    kommenterad kod */
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        setStatusActive();
+        //this.team = new Team();
+    }
+
+    public String getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(String userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatusInActive() {
+        this.status = Status.Inactive;
+    }
+
+    public void setStatusActive() {
+        this.status = Status.Active;
+    }
+
+    /*public void setTeam(User user) {
+        this.team = team.addUser(this);
+    }*/
+
+    public Team getTeam() {
+        return team;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other instanceof User) {
+            final User otherUser = (User) other;
+
+            return userNumber.equals(otherUser.userNumber) && status.equals(otherUser.getStatus()) && team.getTeamName().equals(otherUser.getTeam().getTeamName());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+            result += 17 * userNumber.hashCode();
+            result += 17 * status.hashCode();
+            result += 17 * team.getTeamName().hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
 }
---
+++ src/main/java/se/threegirlsoneflop/springdatajpa/service/UserRepository.java
@@ -0,0 +1,22 @@
+package se.threegirlsoneflop.springdatajpa.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import se.threegirlsoneflop.springdatajpa.model.User;
+
+import javax.persistence.Column;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by josefinelantz on 29/01/16.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    Collection<User> findByUserNumber(String userNumber);
+    Collection<User> findByFirstName(String firstName);
+    Collection<User> findByLastName(String value);
+    Collection<User> findByFirstNameAndLastName(String firstName, String lastName);
+
+    @Query("select u from #{#entityName} u where u.team = ?1")
+    List<User> findAllByTeam(String teamName);
+}
