9d068a2e12649baa93719867015dd2de3d14c618
Joanne Nori <joanne_nori@hotmail.com>
2016-01-30 22:47:52 +0100

    added missing files

--- src/main/java/se/threegirlsoneflop/springdatajpa/service/TeamRepository.java
+++ src/main/java/se/threegirlsoneflop/springdatajpa/service/TeamRepository.java
@@ -3,9 +3,14 @@ package se.threegirlsoneflop.springdatajpa.service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.threegirlsoneflop.springdatajpa.model.Team;
 
+import java.util.Collection;
+
 /**
  * Created by joanne on 28/01/16.
  */
-public interface TeamRepository extends JpaRepository<Team, Long> {
+ interface TeamRepository extends JpaRepository<Team, Long> {
+
+    Collection<Team> findByTeamName(String teamName);
+    Collection<Team> findByStatus(String status);
 
 }
--- src/test/java/se/threegirlsoneflop/springdatajpa/model/TeamTest.java
+++ src/test/java/se/threegirlsoneflop/springdatajpa/model/TeamTest.java
@@ -1,10 +1,35 @@
 package se.threegirlsoneflop.springdatajpa.model;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by joanne on 30/01/16.
  */
 public class TeamTest {
+
+    private Team team1;
+    private Team equalTeam;
+
+
+    @Before
+    public void setup(){
+        team1 = new Team("dreamteam");
+        equalTeam = new Team("dreamteam");
+    }
+
+    @Test
+    public void teamsWithEqualParametersShouldBeEqual(){
+        assertEquals(team1, equalTeam);
+    }
+
+    @Test
+    public void equalTeamsShouldProduceSameHashCode(){
+        assertEquals(team1.hashCode(), equalTeam.hashCode());
+    }
+
+
 
 }
